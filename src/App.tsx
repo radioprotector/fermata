df56dc329afb123e3592e224463198eaf9d140ce
Cate Stock <radioprotector@users.noreply.github.com>
2022-01-21 00:54:15 -0500

    get audio playing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import { useState } from 'react';
+import * as Tone from 'tone';
+
 import './App.css';
+import MusicPlayer from './MusicPlayer';
 
 function App(): JSX.Element {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const handlePlayToggle = async () => {
+    if (!isPlaying) {
+      await Tone.start();
+      setIsPlaying(true);
+      Tone.Transport.start();
+    }
+    else {
+      setIsPlaying(false);
+      Tone.Transport.pause();
+    }
+  };
+  
   return (
     <div>
-      
+      <button
+        type="button"
+        onClick={handlePlayToggle}
+      >
+        {isPlaying && "Stop"}
+        {!isPlaying && "Start"}
+      </button>
+      {isPlaying && <MusicPlayer />}
     </div>
   );
 }
