1e3bdd4fc543ac12bd0287e0cb9b2cbfb85d329c
Cate Stock <radioprotector@users.noreply.github.com>
2022-09-24 16:11:54 -0400

    remove drum code

--- src/ToneManager.ts
+++ src/ToneManager.ts
@@ -1,70 +1,14 @@
-import { Frequency, ToneAudioNode, ToneAudioNodeOptions, Synth, PolySynth, SynthOptions, Sampler, Volume, Gain, CrossFade, Loop, Pattern, Sequence, Context } from 'tone';
+import { Frequency, ToneAudioNode, ToneAudioNodeOptions, Synth, PolySynth, SynthOptions, Volume, Gain, CrossFade, Loop, Context } from 'tone';
 
 // Import globals with specific aliases to avoid https://github.com/Tonejs/Tone.js/issues/1102
 import { loaded as toneLoaded, getDestination as toneGetDestination, getTransport as toneGetTransport, setContext as toneSetContext } from 'tone';
 import { RecursivePartial } from 'tone/build/esm/core/util/Interface';
 import { Instrument, InstrumentOptions } from 'tone/build/esm/instrument/Instrument';
 import { Frequency as FrequencyUnit } from 'tone/build/esm/core/type/Units';
-import { Note } from 'tone/build/esm/core/type/NoteUnits';
 // import { Effect, EffectOptions } from 'tone/build/esm/effect/Effect';
 // import { StereoEffect, StereoEffectOptions } from 'tone/build/esm/effect/StereoEffect';
 
 import * as cst from './constants';
-
-const Rest: Note = "A0";
-
-const Bass: Note = "A1";
-const Snare: Note = "A2";
-
-const LowTom: Note = "A3";
-const MidTom: Note = "D3";
-const HighTom: Note = "A4";
-
-const LowConga: Note = "A5";
-const MidConga: Note = "D5";
-const HighConga: Note = "A6";
-
-const ClosedHat: Note = "A7";
-const OpenHat: Note = "D7";
-const Crash: Note = "A8";
-
-function buildDrumSampler(destinationNode: ToneAudioNode<ToneAudioNodeOptions>): [ instrument: Sampler, output: Volume ] {
-  const sampler = new Sampler({
-    urls: {
-      [Bass]: "BD2500.mp3",
-      [Snare]: "SD2500.mp3",
-      
-      [LowTom]: "LT25.mp3",
-      [MidTom]: "MT25.mp3",
-      [HighTom]: "HT25.mp3",
-
-      [LowConga]: "LC25.mp3",
-      [MidConga]: "MC25.mp3",
-      [HighConga]: "HC25.mp3",
-
-      [ClosedHat]: "CH.mp3",
-      [OpenHat]: "OH25.mp3",
-      [Crash]: "CY2500.mp3"
-    },
-    baseUrl: process.env.PUBLIC_URL + '/assets/',
-  });
-
-   // Create a volume node and connect it to the main destination
-  const volumeOutput = new Volume(-25);
-  volumeOutput.connect(destinationNode);
-
-  return [sampler, volumeOutput]; 
-}
-
-const drumPatternsArray: (Note | Note[])[][] = [
-  [Bass, Rest, Bass, Rest, Bass, Rest, Bass, Rest],
-  [Bass, Rest, [LowTom, Bass], Rest, [MidTom, Bass], Rest, [HighTom, Bass], Snare],
-  [Bass, Rest, [LowTom, Bass], Snare, [MidTom, Bass], Snare, [LowTom, Bass], Snare]
-  // [Bass, Rest, LowTom, Rest, Snare, Rest, LowTom, Rest, Bass, Rest, LowTom, Rest, Snare, Rest, MidTom, HighTom],
-  // [Bass, Rest, [ClosedHat, Snare], Rest, Bass, Rest, [ClosedHat, Snare], Rest, Bass, OpenHat, Bass, Rest, Crash, HighTom, MidTom, LowTom]
-];
-
-const fullDrumSequence = drumPatternsArray.flat(1);
 
 function buildCloudChain(cloudIdx: number, destinationNode: ToneAudioNode<ToneAudioNodeOptions>): CloudAudioChain {
   // Get the period for this cloud
@@ -124,10 +68,6 @@ function buildCloudChain(cloudIdx: number, destinationNode: ToneAudioNode<ToneAu
   };
 }
 
-
-const blankSequence = new Sequence({ events: [Rest, Rest] as Note[] });
-const blankPattern = new Pattern({ values: [Rest, Rest] as Note[] });
-
 /**
  * Describes a tone chain for a particular boid cloud.
  */
@@ -158,6 +98,8 @@ class ToneManager {
 
   private readonly chainReceiverNode: Gain;
 
+  private patternsInitialized: boolean = false;
+
   private _isPlaying: boolean = false;
 
   get isPlaying(): boolean {
@@ -176,14 +118,16 @@ class ToneManager {
     for(let cloudIdx = 0; cloudIdx < cst.CLOUD_COUNT; cloudIdx++) {
       this.cloudChains.push(buildCloudChain(cloudIdx, this.chainReceiverNode));
     }
-
-    // // Drum
-    // [this.drumInstrument, this.drumVolume] = buildDrumSampler();
   }
   
-  public registerPatterns = async (shouldOverwrite: boolean = true): Promise<void> => {
+  public registerPatterns = async (): Promise<void> => {
     return toneLoaded()
       .then(() => {
+        if (this.patternsInitialized) {
+          return;
+        }
+
+        // Create loops for all of the cloud chains
         for(const cloudChain of this.cloudChains) {
           new Loop(() => {
             cloudChain.baseInstrument.triggerAttackRelease(cloudChain.baseNote, cloudChain.periodSeconds * 1.05);
@@ -194,27 +138,14 @@ class ToneManager {
         // Connect the chain receiver node to the destination
         this.chainReceiverNode.toDestination();
 
-      // // See if we need to initialize or overwrite the drums
-      // if (!this.drumPatternInitialized || shouldOverwrite) {
-      //   // Dispose existing drum
-      //   if (this.drumPatternInitialized) {
-      //     this.drumPattern.dispose();
-      //   }
-
-      //   this.drumPattern = new Tone.Sequence((time, note) => {
-      //     if (note !== Rest) {
-      //       this.drumInstrument.triggerAttackRelease(note, 1, time);
-      //     } 
-      //   }, fullDrumSequence);
-
-      //   this.drumPatternInitialized = true;
-      // }
+        // Indicate that the patterns have been initialized
+        this.patternsInitialized = true;
     });
   }
 
   public startPlayback = async (): Promise<void> => {
     // Ensure patterns are good to go
-    await this.registerPatterns(false);
+    await this.registerPatterns();
     
     toneGetDestination().mute = false;
 
@@ -222,17 +153,10 @@ class ToneManager {
     toneTransport.bpm.value = 100;
     toneTransport.start();
 
-    // // Start all patterns
-    // this.drumPattern.start(0);
     this._isPlaying = true;
   }
  
   public stopPlayback = (): void =>  {
-    // // Stop all sequences
-    // if (this.drumPattern.state === 'started') {
-    //   this.drumPattern.stop();
-    // }
-
     toneGetDestination().mute = true;
     toneGetTransport().pause();
 
@@ -241,11 +165,6 @@ class ToneManager {
 
   public dispose = (): void => {
     this.stopPlayback();
-
-    // if (this.drumPatternInitialized) {
-    //   this.drumPattern.dispose();
-    //   this.drumPatternInitialized = false;
-    // }
   }
 }
 
