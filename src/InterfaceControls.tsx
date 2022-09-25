52a48c63c8604ee0c04f2d945466bc64a12daf48
Cate Stock <radioprotector@users.noreply.github.com>
2022-09-25 16:05:49 -0400

    Add orbit speed control

--- src/InterfaceControls.tsx
+++ src/InterfaceControls.tsx
@@ -13,6 +13,8 @@ export interface InterfaceControlsProps {
 }
 
 function InterfaceControls(props: InterfaceControlsProps): JSX.Element {
+  const rotationSpeedIndex = useFermataStore((state) => state.rotationSpeedIndex);
+  const cycleRotationSpeed = useFermataStore((state) => state.cycleRotation);
   const isAudioPlaying = useFermataStore((state) => state.isAudioPlaying);
   const setAudioPlaying = useFermataStore((state) => state.setAudioPlaying);
 
@@ -32,6 +34,23 @@ function InterfaceControls(props: InterfaceControlsProps): JSX.Element {
 
   return (
     <div id="control-items">
+      {/**
+        * Because this is using flex row-reverse order, the items that show up first will be on the far right. 
+        */}
+      <button
+        type="button"
+        title="Cycle orbit speed"
+        onClick={cycleRotationSpeed}
+      >
+        <span
+          className="fa-layers fa-fw"
+        >
+          <FontAwesomeIcon icon="arrow-rotate-forward" transform="shrink-1 left-6" />
+          {rotationSpeedIndex == 0 && <FontAwesomeIcon icon="pause" transform="shrink-6 right-8" />}
+          {rotationSpeedIndex == 1 && <FontAwesomeIcon icon="play" transform="shrink-6 right-8" />}
+          {rotationSpeedIndex == 2 && <FontAwesomeIcon icon="forward" transform="shrink-6 right-8" />}
+        </span>
+      </button>
       <button
         type="button"
         title="Toggle audio"
